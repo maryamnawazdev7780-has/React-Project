c7deecafd8ad90c64b45620e7cbca4f936146e94
Your Name <your_email@gmail.com>
2026-01-24 13:48:06 +0500

    Prepare for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-project/',  // <-- This should match your GitHub repo name
   plugins: [react()],
 })
